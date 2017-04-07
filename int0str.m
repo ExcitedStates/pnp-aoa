df7321999d58f9b4b02a17b4aed004f2a8956dbf
leeneil <lishantreasure@gmail.com>
2017-04-07 13:01:22 -0700

    add int0str

---
+++ int0str.m
@@ -0,0 +1,11 @@
+function str = int0str(num, lg)
+if num <0
+    str = num2str(num);
+else
+    if num == 0
+        str = repmat('0', 1, lg);
+    else
+        str = [repmat('0', 1, (lg - floor(log10(num)) - 1) ) num2str(num)];
+    end
+end
+end
